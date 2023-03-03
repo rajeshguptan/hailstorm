a5e2331a334de19c1e664e8dacbbdb64d1129235
rajesh gupta <rajesh.guptan@gmail.com>
2023-03-02 21:29:15 -0800

    Try release3

---
+++ src/main/java/app/hailstorm/reporter/HtmlSummaryReporter.java
@@ -0,0 +1,19 @@
+package app.hailstorm.reporter;
+
+import app.hailstorm.stats.Stat;
+
+public class HtmlSummaryReporter implements StatReporter {
+
+	@Override
+	public void reportRunningStat(Stat runningStats) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reportSummaryStat(Stat summaryStats) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
